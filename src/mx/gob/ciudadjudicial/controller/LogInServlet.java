a55d5670d49013f95bc1b349886c9b29f0bbb4a1
Angel Castro <angel.c.g.027@gmail.com>
2019-05-28 18:25:44 -0500

    otra oportunidad

---
+++ src/mx/gob/ciudadjudicial/controller/LogInServlet.java
@@ -0,0 +1,102 @@
+package mx.gob.ciudadjudicial.controller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogInServlet
+ */
+@WebServlet(description = "Servlet para login", urlPatterns = { "/LogInServlet" })
+public class LogInServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Properties props = new Properties();
+		String archivo = "config.properties";
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(archivo);
+		
+		if(inputStream != null) {
+			props.load(inputStream);
+		}else {
+			throw new FileNotFoundException("Property file "+ archivo +" no se encontró en el classPath");
+		}
+		
+		String urlServidor = props.getProperty("urlServidor");
+		String usuario = props.getProperty("user");
+		String passw = props.getProperty("password");
+		String driver = props.getProperty("driver");
+		
+		String correo = request.getParameter("txtCorreo");
+		String password = request.getParameter("txtPassword");
+		String correoSQL = "";
+		String passwordSQL = "";
+		String nombreSQL = "";
+		String apellidoSQL = "";
+		
+		Connection conn = null;
+		Statement stmnt = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			Class.forName(driver).getDeclaredConstructor().newInstance();
+			conn = DriverManager.getConnection(urlServidor, usuario, passw);
+			stmnt = conn.createStatement();
+			
+			rs = stmnt.executeQuery(" SELECT * FROM ciudad_judicial.usuarios WHERE email = \""+ correo +"\"");
+			
+			rs.next();
+			
+			correoSQL = rs.getString("email");
+			passwordSQL = rs.getString("password");
+			nombreSQL = rs.getString("nombre");
+			apellidoSQL = rs.getString("apellidoP");
+			
+			if(correo.equals(correoSQL)&&password.equals(passwordSQL)) {
+				HttpSession objetoSesion=request.getSession(true);
+				objetoSesion.setAttribute("Usuario", correo);
+				objetoSesion.setAttribute("Password", password);
+				objetoSesion.setMaxInactiveInterval(30);
+				response.sendRedirect("home.html");
+			}else {
+				PrintWriter out=response.getWriter();
+				RequestDispatcher dis=request.getRequestDispatcher("index.html");
+				out.println("<font color='red'>Usuasio o contraseña no válidos, intente de nuevo</font>");
+				dis.include(request, response);
+			}
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}finally {
+			try {
+				rs.close();
+				conn.close();
+				stmnt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
---
+++ src/mx/gob/ciudadjudicial/controller/SigninServlet.java
@@ -0,0 +1,97 @@
+package mx.gob.ciudadjudicial.controller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SignInServlet
+ */
+@WebServlet("/SigninServlet")
+public class SigninServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Properties props = new Properties();
+		String archivo = "config.properties";
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(archivo);
+		
+		if(inputStream != null) {
+			props.load(inputStream);
+		}else {
+			throw new FileNotFoundException("Property file "+ archivo +" no se encontró en el classPath");
+		}
+		
+		String urlServidor = props.getProperty("urlServidor");
+		String usuario = props.getProperty("user");
+		String passw = props.getProperty("password");
+		String driver = props.getProperty("driver");
+		
+		String nombre = request.getParameter("txtNombre");
+		String apellidoP = request.getParameter("txtApellidoP");
+		String apellidoM = request.getParameter("txtApellidoM");
+		String fechaNacimiento = request.getParameter("txtBirth"); 
+		String sexo = request.getParameter("txtSexo");
+		String correo = request.getParameter("txtCorreo");
+		String password = request.getParameter("txtPassword");
+		int nRegistros = 0;
+		
+		Connection conn = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			Class.forName(driver).getDeclaredConstructor().newInstance();
+			conn = DriverManager.getConnection(urlServidor, usuario, passw);
+			
+			String query ="INSERT INTO ciudad_judicial.usuarios (nombre, apellidoP, apellidoM, fechaNacimiento, sexo, email, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+			
+			PreparedStatement pstmnt = conn.prepareStatement(query);
+			pstmnt.setString(1, nombre);
+			pstmnt.setString(2, apellidoP);
+			pstmnt.setString(3, apellidoM);
+			pstmnt.setString(4, fechaNacimiento);
+			pstmnt.setString(5, sexo);
+			pstmnt.setString(6, correo);
+			pstmnt.setString(7, password);
+			
+			nRegistros = pstmnt.executeUpdate();
+			
+			if(nRegistros > 0) {
+				
+				response.getWriter().append("Registro añadido");
+			}else {
+				response.getWriter().append("Registro NO añadido");
+			}
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}finally {
+			try {
+				rs.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
